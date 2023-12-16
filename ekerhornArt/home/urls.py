e57de7a96a582d838571759ed2e96733d9d3a0bc
Amycus89 <amycus89@gmail.com>
2023-12-16 14:17:52 +0100

    Set up form in Django and updated CSS to reflect this

--- ekerhornArt/home/urls.py
+++ ekerhornArt/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views # From current (.) directory, import views.py
 
+app_name = "home"
+
 urlpatterns = [
 path("", views.index, name="index")
 ]
--- ekerhornArt/home/views.py
+++ ekerhornArt/home/views.py
@@ -1,6 +1,27 @@
+from django import forms
 from django.shortcuts import render
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Namn', label_suffix='', max_length=70)
+    email = forms.EmailField(label='E-Mail', label_suffix='')
+    message = forms.CharField(label='Meddelande', label_suffix='', widget=forms.Textarea, max_length=500)
 
 # Create your views here.
 def index(request):
-    # Return the index.html in ekerhornArt/home/templates/home/index.html
-    return render(request, "home/index.html")
+    if request.method == "POST": # If submit button was used
+        # This will save all the user input of the form inside the variable "form":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            print(name, email, message)
+        else:
+            render(request, "home/index.html", {
+			"form": form # Will still show previously entered info, alongside error messages
+		})
+
+    # GET method
+    return render(request, "home/index.html", {
+	"form": ContactForm()
+})
