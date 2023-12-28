59998ac3b582760b3fc930969f64825a3c81fbab
Amycus89 <amycus89@gmail.com>
2023-12-28 22:59:37 +0100

    Edited Events modal, adjusted layout html and css

--- ekerhornArt/events/models.py
+++ ekerhornArt/events/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=46)
-    background = models.ImageField(upload_to="images/")
+    background = models.ImageField(upload_to="events/static/events/images")
     description = models.TextField()
     date = models.DateField()
     location_name = models.CharField(max_length=46)
     street = models.CharField(max_length=46)
     city = models.CharField(max_length=46)
-    download_file = models.FileField(upload_to="files/", blank=True)
+    download_file = models.FileField(upload_to="events/static/events/files", blank=True)
     read_more = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date}"
--- ekerhornArt/events/views.py
+++ ekerhornArt/events/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Event
+
 # Create your views here.
 def index(request):
-    return render(request, "events/index.html")
+    return render(request, "events/index.html", {
+        "events": Event.objects.all()
+    })
