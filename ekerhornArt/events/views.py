e8d805cdd917073fd11b7c2b3b3c13c5d4a3c732
Amycus89 <amycus89@gmail.com>
2023-12-31 23:41:19 +0100

    Altered behaviour of navigation cta button

--- ekerhornArt/events/views.py
+++ ekerhornArt/events/views.py
@@ -16,32 +16,8 @@ class ContactForm(forms.Form):
 
 # Create your views here.
 def index(request):
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest': # If submit button was used
-        # This will save all the user input of the form inside the variable "form":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            message = form.cleaned_data["message"]
-
-            #Send email EMAIL_HOST
-            subject = f"{name} har kontaktat dig ang Ekerhorn Art"
-            body = f"Email: {email}\n\n{message}"
-            recipient = f"Ekerhorn Art <{env('myEmail')}>"
-            sender = f"{name} <{email}>"
-
-            send_mail(subject, body, sender, [recipient], fail_silently=False) # Remove fail_silently once done testing
-
-            return JsonResponse({
-                "success": True,
-                "name": name
-                })
-        else:
-            return JsonResponse({"success": False, "errors": form.errors}, status=400)
-
     return render(request, "events/index.html", {
-        "events": Event.objects.all(),
-        "form": ContactForm()
+        "events": Event.objects.all()
     })
 
 def event(request, event_id):
