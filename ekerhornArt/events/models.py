45235eaf6cd94b15f3c26a4514928d92bbfad17a
Amycus89 <amycus89@gmail.com>
2024-02-03 20:19:27 +0100

    Made all resized event images dynamically generated

--- ekerhornArt/events/models.py
+++ ekerhornArt/events/models.py
@@ -3,13 +3,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from PIL import Image
 import os
+from django.conf import settings
 
 # Create your models here.
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=46)
-    background = models.ImageField(upload_to="")
+    background = models.ImageField(upload_to="events/images")
     tags = models.CharField(max_length=46, blank=True)
     description = models.TextField()
     date = models.DateField()
@@ -21,6 +22,19 @@ class Event(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.date}"
+
+    def get_resized_image_urls(self):
+        event_file_name = self.name.lower().replace(" ", "_").replace("ö", "o").replace("ä", "a").replace("å", "a").translate(str.maketrans("", "", "/:*?!#&\"<>|"))
+        img_p_url = f"{settings.MEDIA_URL}events/images/{self.id}_{event_file_name}_p.jpg"
+        img_s_url = f"{settings.MEDIA_URL}events/images/{self.id}_{event_file_name}_s.jpg"
+        img_m_url = f"{settings.MEDIA_URL}events/images/{self.id}_{event_file_name}_m.jpg"
+        img_l_url = f"{settings.MEDIA_URL}events/images/{self.id}_{event_file_name}_l.jpg"
+        return {
+            'img_p_url': img_p_url,
+            'img_s_url': img_s_url,
+            'img_m_url': img_m_url,
+            'img_l_url': img_l_url,
+        }
 
 #Whenever admin creates new event
 @receiver(post_save, sender=Event)
@@ -42,13 +56,10 @@ def resize_uploaded_image(sender, instance, created, **kwargs):
     img_3 = img.resize((img_3_Width, int(img_3_Width / aspect_ratio)))
 
     # Save the resized images
-    event_file_name = instance.name.replace(" ", "_").lower().translate(str.maketrans("", "", "/:*?!#&\"<>|"))
-    print(event_file_name)
-    img_0.save(f"media/events/images/{event_file_name}_p.jpg")
-    img_1.save(f"media/events/images/{event_file_name}_s.jpg")
-    img_2.save(f"media/events/images/{event_file_name}_m.jpg")
-    img_3.save(f"media/events/images/{event_file_name}_l.jpg")
-
-    # Delete the old image
+    event_file_name = instance.name.lower().replace(" ", "_").replace("ö", "o").replace("ä", "a").replace("å", "a").translate(str.maketrans("", "", "/:*?!#&\"<>|"))
+    img_0.save(f"media/events/images/{instance.id}_{event_file_name}_p.jpg")
+    img_1.save(f"media/events/images/{instance.id}_{event_file_name}_s.jpg")
+    img_2.save(f"media/events/images/{instance.id}_{event_file_name}_m.jpg")
+    img_3.save(f"media/events/images/{instance.id}_{event_file_name}_l.jpg")
+    #Delete it
     os.remove(instance.background.path)
-    print("Image resized and saved")
